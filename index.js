6252fe3b3cf38c03b19e4b5848ed1fdc7ccde687
Furkan <muhammedcengiz1@gmail.com>
2022-09-07 13:35:33 +0300

    Blackjack mantığı tamamlandı. Oyunda can kalmayınca yeni kart alamama, blackjack olmadan veya can bitmeden yeni oyun başlatamama eklendi. Ayrıca kartlar array içinde depolanıyor. Son olarak person diye bir nesne eklendi.

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
+
+let player = {
+    name: "Furkan",
+    chips: 44
+}
+
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el")
 let cardEl = document.getElementById("card-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function renderGame(){
-    startGame();
-}
+    cardEl.textContent = "Card: "
 
-function startGame(){
+    for(let i = 0; i<cards.length; i++){
+        cardEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: "+ sum
-    cardEl.textContent = "Card: "+ cards[0] + " - " + cards[1]
     if (sum <= 20) {
         message = "Do you want to draw a new card ?"
         messageEl.textContent = message;
@@ -29,17 +36,40 @@ function startGame(){
         isAlive = false
         messageEl.textContent = message;
     }
+    messageEl.textContent = message
+}
+
+function startGame(){
+    if(!isAlive || hasBlackJack){
+        let firstCard = getRandomCard()
+        let secondCard = getRandomCard()
+        cards = [firstCard, secondCard]
+        sum = firstCard + secondCard
+        isAlive = true
+        hasBlackJack =false;
+        renderGame();
+    }
 }
 console.log(message)
 
 function newCard(){
-    let card = 6;
-    sum+=card;
-    cards.push(card)
-    renderGame()
-    if(cards[2] === 6 ){
-        console.log("cards[2] === 6")
+
+    if(isAlive && !hasBlackJack){
+        let card = getRandomCard()
+        sum+=card;
+        cards.push(card)
+        renderGame()
     }
-    
 }
 
+function getRandomCard() {
+
+    let rand = Math.floor( Math.random()*13 ) + 1
+    if(rand === 1) {
+        return 11
+    } else if(rand>10) {
+        return 10
+    } else {
+        return rand
+    }
+}
